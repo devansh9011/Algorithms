0b8a900b00c3219eb3849de9e8947591b1210df3
Devansh Sharma <devansh9011@gmail.com>
2020-10-09 16:11:46 +0530

    Added algorithm to find all the divisors af a given number

---
+++ finding_all_divisors.cpp
@@ -0,0 +1,36 @@
+/*
+Problem Statement:-
+	Given a number n, find and print all its divisors in the increasing order
+Solution:-
+	Using mathematics
+Runtime Complexity:-
+	O(sqrt(n)*log(cbrt(n)))
+Space Complexity:-
+	O(cbrt(n)) as the upper bound for the number of divisors is n^(1/3)
+*/
+#include<iostream>
+#include<set>
+
+using namespace std;
+
+int main()
+{
+	cout<<"\nEnter a number : ";
+	int n;
+	cin>>n;
+	set<int> s;
+	s.insert(1);
+	s.insert(n);
+	for(int i=2;i*i<=n;i++)
+		if(n%i==0)
+		{
+			s.insert(i);
+			s.insert(n/i);
+		}
+	cout<<"\nDivisors of the number are : ";
+	for(auto x:s)
+		cout<<x<<" ";
+
+
+	return 0;
+}
